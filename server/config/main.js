cad493f93baf854474204a9e77c9ee441a77fc02
Alexis Martínez Chacón <alexismartinezchacon@gmail.com>
2018-02-07 09:10:59 +0100

    Added docker config to build automatically

--- server/config/main.js
+++ server/config/main.js
@@ -2,7 +2,7 @@ module.exports = {
     // Secret key for JWT signing and encryption
     'secret': 'My_Secret_KEY',
     // Database connection information
-    'database': 'mongodb://localhost:27017/babelomics',
+    'database': 'mongodb://mongodb:27017/babelomics',
     // Setting port for server
     'port': process.env.PORT || 3000  
 }
